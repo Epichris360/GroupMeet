48c6713b643905b84faf3b801d3606c1ccf77150
cristian <epicstorm19@gmail.com>
2018-03-21 16:03:54 -0300

    basic setup of group crud

---
+++ controllers/groupController.js
@@ -0,0 +1,40 @@
+const turbo       = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+const collections = require('../collections')
+const constants   = require('../constants') 
+const functions   = require('../functions')
+
+const createGet = (req, res) => {
+    const vertexSession = req.vertexSession
+    res.render("group/create", { vertexSession })
+    return
+}
+
+const createPost = (req, res) => {
+
+}
+
+const editGet = (req, res) => {
+    const vertexSession = req.vertexSession
+    res.render("group/edit", { vertexSession })
+    return
+}
+
+const editPost = (req, res) => {
+
+}
+
+const show = (req, res) => {
+    const vertexSession = req.vertexSession
+    res.render("group/show", { vertexSession })
+    return
+}
+
+const list = (req, res) => {
+    const vertexSession = req.vertexSession
+    res.render("group/list", { vertexSession })
+    return
+}
+
+module.exports = {
+    createGet, createPost, editGet, editPost, show, list
+}
